34b8153f7cec32aedff80c647093fcdea1d4d4cf
emircankilicaslan <103359798+emircankilicaslan@users.noreply.github.com>
2024-10-07 19:19:11 +0300

    BookTrack
    This is a full-stack web application that utilizes .NET for the backend, MSSQL for the database, and Angular for the frontend.

--- LibraryApp/LibraryApp/obj/Debug/net8.0/LibraryApp.AssemblyInfo.cs
+++ LibraryApp/LibraryApp/obj/Debug/net8.0/LibraryApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbbeb9abbadcd0cb1b8b2fe0d0653079351bac66")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
